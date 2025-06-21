023fa783b67958f07fd3ec6165ee42c076226501
Kingsley Enyinnaya <enyikings3778@gmail.com>
2025-06-21 20:04:41 +0000

    use favicon-32x32.png as favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=swap" rel="stylesheet" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon-32x32.png" type="image/png" />
         <meta name="theme-color" content="#E11D48" />
       </head>
       <body className="font-body antialiased bg-background">
